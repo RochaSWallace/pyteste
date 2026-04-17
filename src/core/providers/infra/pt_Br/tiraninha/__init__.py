f68e00d79fb508c8948b905d94b9cba6d13f53cb
Wallace <wallacesouzarocha@gmail.com>
2026-04-17 20:29:56 -0300

    att2 imperi

--- src/core/providers/infra/pt_Br/tiraninha/__init__.py
+++ src/core/providers/infra/pt_Br/tiraninha/__init__.py
@@ -17,21 +17,13 @@ class TiraninhaProvider(WordPressMadara):
     name = 'Tiraninha'
     lang = 'pt_Br'
     domain = ['tiraninha.world']
-<<<<<<< HEAD
-
-    def __init__(self):
-        self.url = 'https://tiraninha.world/'
-=======
     has_login = True
 
     def __init__(self):
-        super().__init__()
         self.url = 'https://tiraninha.world/'
-        self.login_url = urljoin(self.url, 'wp-login.php')
->>>>>>> 1b5c38cb1617ea1c3a35a5d35d2eab1cc66f1c97
 
         self.path = ''
-        
+        self.login_url = urljoin(self.url, 'wp-login.php')
         self.query_mangas = 'div.post-title h3 a, div.post-title h5 a'
         self.query_chapters = 'li.wp-manga-chapter > a'
         self.query_chapters_title_bloat = None
